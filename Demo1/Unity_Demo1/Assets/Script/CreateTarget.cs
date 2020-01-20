e99682c7b27f2e72a6edbed7fc63486fe4fd2350
franklin0913 <frank30719@gmail.com>
2020-01-20 13:36:58 +0800

    新增Unity Demo1

---
+++ Demo1/Unity_Demo1/Assets/Script/CreateTarget.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateTarget : MonoBehaviour
+{
+    private float Timer = 0;
+    public GameObject Target;
+    private GameObject TargetClone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        if (Timer >= 2)
+        {
+            float NewX = Random.Range(-4, 4);
+            float NewZ = Random.Range(-4, 4);
+            TargetClone = Instantiate(Target, new Vector3(NewX, 0, NewZ), Target.transform.rotation);
+            Timer = 0;
+        }
+    }
+}
