96a2a50cd33856ed49a4c332180cf7c76b00203e
franklin0913 <frank30719@gmail.com>
2020-01-29 14:29:08 +0800

    新增Player完整結構及增加移動豐富性

--- Demo1/Unity_Demo1/Assets/Script/PlayerMove.cs
+++ Demo1/Unity_Demo1/Assets/Script/PlayerMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    private readonly float Speed=0.1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,21 +14,26 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        transform.rotation = new Quaternion(0, 0, 0, 1);
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.position = transform.position + Vector3.forward;
+            transform.position = transform.position + Vector3.forward * Speed;
+            transform.rotation = new Quaternion(0.1f, 0, 0, 1);
         }
-        else if (Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S))
         {
-            transform.position = transform.position + Vector3.back;
+            transform.position = transform.position + Vector3.back * Speed;
+            transform.rotation = new Quaternion(-0.1f, 0, 0, 1);
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
-            transform.position = transform.position + Vector3.right;
+            transform.position = transform.position + Vector3.right * Speed;
+            transform.rotation = new Quaternion(0, 0, -0.1f, 1);
         }
-        else if (Input.GetKeyDown(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))
         {
-            transform.position = transform.position + Vector3.left;
+            transform.position = transform.position + Vector3.left * Speed;
+            transform.rotation = new Quaternion(0, 0, 0.1f, 1);
         }
     }
 }
