187cbe5916c9b4c6e48f2b0084f097c52d08beba
franklin0913 <frank30719@gmail.com>
2020-03-23 01:11:28 +0800

    Revert "新增Skybox及移動平台"
    This reverts commit a7077f037fadfee8f10605960f5c6c2e2f33fb2e.

--- Unity_Demo3/Assets/Scripts/PlayerAction.cs
+++ Unity_Demo3/Assets/Scripts/PlayerAction.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class PlayerAction : MonoBehaviour
 {
     public Animation _anim;
-    private bool _isZaxis = false, _isXaxis = false, _isCrash = false;
-    private float start, end;
+    private bool _isZaxis = false, _isXaxis = false;
+    private int start, end;
     private float _starttime;
     private float lerpvalue;
-    public float _speed;
+    public float _speed=1;
 
     // Start is called before the first frame update
     void Start()
@@ -22,68 +22,55 @@ public class PlayerAction : MonoBehaviour
     {
         if (Input.GetKeyDown("w"))
         {
-            CatchPlayer._isCatch = false;
             _anim.Play("Jump");
             _isZaxis = true;
-            _isXaxis = false;
             _starttime = Time.time;
-            start = transform.position.z;
+            start = (int)transform.position.z;
             end = start + 1;
-            _isCrash = false;
         }
         else if (Input.GetKeyDown("s"))
         {
-            CatchPlayer._isCatch = false;
             _anim.Play("Jump");
             _isZaxis = true;
-            _isXaxis = false;
             _starttime = Time.time;
-            start = transform.position.z;
+            start = (int)transform.position.z;
             end = start - 1;
-            _isCrash = false;
         }
         else if (Input.GetKeyDown("d"))
         {
-            CatchPlayer._isCatch = false;
             _anim.Play("Jump");
-            _isZaxis = false;
             _isXaxis = true;
             _starttime = Time.time;
-            start = transform.position.x;
+            start = (int)transform.position.x;
             end = start + 1;
-            _isCrash = false;
         }
         else if (Input.GetKeyDown("a"))
         {
-            CatchPlayer._isCatch = false;
             _anim.Play("Jump");
-            _isZaxis = false;
             _isXaxis = true;
             _starttime = Time.time;
-            start = transform.position.x;
+            start = (int)transform.position.x;
             end = start - 1;
-            _isCrash = false;
         }
-        if (_isZaxis && !_isCrash)
+        if (_isZaxis)
         {
             lerpvalue = Mathf.Lerp(start, end, (Time.time - _starttime) * _speed);
             transform.position = new Vector3(transform.position.x, transform.position.y, lerpvalue);
             if (transform.position.z == end) _isZaxis = false;
         }
-        if (_isXaxis && !_isCrash)
+        if (_isXaxis)
         {
             lerpvalue = Mathf.Lerp(start, end, (Time.time - _starttime) * _speed);
             transform.position = new Vector3(lerpvalue, transform.position.y, transform.position.z);
             if (transform.position.x == end) _isXaxis = false;
         }
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "Obstacle")
         {
-            _isCrash = true;
-            gameObject.transform.position = new Vector3(0, 0.475f, 0);
             Debug.Log("撞到障礙物了!!!");
         }
     }
